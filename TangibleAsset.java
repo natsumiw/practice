afc2d623b1df28ecdaf67bdc2b2558f95b07ec45
natsumiw <natsumiw1002@gmail.com>
2022-10-11 11:39:46 +0900

    Create Book.java, modify TangibleAsset.java

--- TangibleAsset.java
+++ TangibleAsset.java
@@ -12,5 +12,5 @@ public abstract class TangibleAsset {
   public String getName() { return this.name; }
   public int getPrice() { return this.price; }
   public String getColor() { return this.color; }
-  
+  // test  
 }
